1ad8f1872e6344418ec805e7ef8b5bc42bd8be33
Ing.Dan <Ing.Dan@ISC>
2015-03-18 20:07:47 -0600

    proyecto final base de datos 2

---
+++ src/com/capitulo6/threads/InterfazReloj.java
@@ -0,0 +1,62 @@
+
+package com.capitulo6.threads;
+
+import java.util.Calendar;
+
+public class InterfazReloj extends javax.swing.JFrame {
+
+
+    public InterfazReloj() {
+        initComponents();
+        
+        while(true){
+        Calendar cal = Calendar.getInstance();
+                String time = cal.get(Calendar.HOUR_OF_DAY) + ":" + cal.get(Calendar.MINUTE) + ":" + cal.get(Calendar.SECOND);
+
+                EtiquetaReloj.setText("la hr es: " + time);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        EtiquetaReloj = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        EtiquetaReloj.setText("00:00:00");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(81, 81, 81)
+                .addComponent(EtiquetaReloj)
+                .addContainerGap(285, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(85, 85, 85)
+                .addComponent(EtiquetaReloj)
+                .addContainerGap(201, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    public static void main(String args[]) {
+
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new InterfazReloj().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel EtiquetaReloj;
+    // End of variables declaration//GEN-END:variables
+}
--- src/com/capitulo6/threads/relojito.java
+++ src/com/capitulo6/threads/relojito.java
@@ -6,20 +6,20 @@ import java.util.*;
 public class relojito  extends Thread{
     
     public void run(){
-        while(true){        //ciclo inmortal
-        
-        try{
-            Calendar cal = Calendar.getInstance();
-            int hora = cal.get(Calendar.HOUR_OF_DAY);
-            int minuto = cal.get(Calendar.MINUTE);
-            int segundo = cal.get(Calendar.SECOND);
-            
-            System.out.println("la hora es " + hora + ":" + minuto + ":" + segundo);
-            
-            Thread.sleep(1000);
-            
-            
-        }catch(Exception e){}
+            while(true){        //ciclo inmortal
+
+            try{
+                Calendar cal = Calendar.getInstance();
+                int hora = cal.get(Calendar.HOUR_OF_DAY);
+                int minuto = cal.get(Calendar.MINUTE);
+                int segundo = cal.get(Calendar.SECOND);
+
+                System.out.println("la hora es " + hora + ":" + minuto + ":" + segundo);
+
+                Thread.sleep(1000);
+
+
+            }catch(Exception e){}
         
         }
     }
